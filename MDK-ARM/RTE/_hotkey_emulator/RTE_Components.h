2e273ec993f04d947ca229edbfcbe7bfe5d2de22
Yury Vostrenkov <yuravostrenkov@gmail.com>
2021-06-01 15:14:25 +0300

    Added buttons periphery

---
+++ MDK-ARM/RTE/_hotkey_emulator/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'hotkey_emulator' 
+ * Target:  'hotkey_emulator' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
